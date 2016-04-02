c41f237b02dbbe16d5ab61f6dc639e2cfeb8d065
brodieG <brodieG@users.noreply.github.com>
2016-04-02 13:58:35 -0400

    testup; adding diff_chr file

--- tests/testthat/testthat.core.R
+++ tests/testthat/testthat.core.R
@@ -1,26 +1,6 @@
 library(diffobj)
 
 stop("Add tests for following strings described in https://neil.fraser.name/writing/diff/")
-# Corner cases from https://neil.fraser.name/writing/diff/
-# Both of these appear handled correctly by the algorithm here
-# first one: suboptimal edit script due to two sided approach
-A1 <- c("X", "A", "X", "C", "X", "A", "B", "C")
-B1 <- c("A", "B", "C", "Y")
-diff_chr(A1, B1)
-
-# second one: failure to find intersection at ends of paths (paths run into
-# each other eventually)
-
-A2 <- c("A", "B", "X", "A", "B")
-B2 <- c("A", "Y", "B")
-diff_chr(A2, B2)
-
-set.seed(1)
-X <- do.call(paste0, expand.grid(LETTERS, LETTERS, LETTERS))
-
-diff_chr(X[1:2000], X[2001:4000])
-diff_chr(X[1:5000], X[5001:10000])
-
 local({
   # The Myers paper strings
 
