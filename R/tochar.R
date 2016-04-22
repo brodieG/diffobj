2069fce09ee044f3e57eb3046111f5326f0aaee9
brodieG <brodieG@users.noreply.github.com>
2016-04-21 22:08:20 -0400

    padding happens before text style

--- R/tochar.R
+++ R/tochar.R
@@ -425,7 +425,7 @@ setMethod("as.character", "diffObjDiff",
     # all elements are same length
 
     pre.render.w <- replicate(
-      2L, vector("list", length(pre.render.s[[1L]])), simplify=FALSE
+      2L, vector("list", length(pre.render[[1L]]$dat)), simplify=FALSE
     )
     for(i in seq_along(pre.render.w)) {
       hdr <- pre.render[[i]]$type == "header"
@@ -471,29 +471,27 @@ setMethod("as.character", "diffObjDiff",
     # Apply text level styles
 
     es <- x@etc@style
-    pre.render.s <- lapply(
-      pre.render.w.p,
-      function(z) {
-        res <- character(length(z$dat))
-        res[z$type == "insert"] <-
-          es@text(es@text.insert(z$dat[z$type == "insert"]))
-        res[z$type == "delete"] <-
-          es@text(es@text.delete(z$dat[z$type == "delete"]))
-        res[z$type == "match"] <-
-          es@text(es@text.match(z$dat[z$type == "match"]))
-        res[z$type == "header"] <- es@header(z$dat[z$type == "header"])
-        if(mode == "context") {
-          ctx.sep <- es@context.sep(es@context.sep.txt)
-          res[z$type == "context.sep"] <- ctx.sep
-        }
+    funs.ts <- list(
+      insert=function(x) es@text(es@text.insert(x)),
+      delete=function(x) es@text(es@text.delete(x)),
+      match=function(x) es@text(es@text.match(x)),
+      header=es@header, context.sep=es@context.sep
+    )
+    pre.render.s <- Map(
+      function(dat, type) {
+        res <- vector("list", length(dat))
+        res[type == "context.sep"] <- list(es@context.sep.txt)
+        for(i in names(funs.ts))
+          res[type == i] <- lapply(dat[type == i], funs.ts[[i]])
         res
-      }
+      },
+      pre.render.w.p, types
     )
     # Apply layout; output should be character with each element representing
     # a row of output
 
     out <- table_ascii(
-      banner.del=banner.A, banner.ins=banner.B, cols=pre.render.w.p,
+      banner.del=banner.A, banner.ins=banner.B, cols=pre.render.s,
       gutters=gutters, pads=pads, types=types, etc=x@etc
     )
     # Finalize
