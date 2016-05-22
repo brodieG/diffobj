fd2cc63c5a878f4697085feec80b262750e72462
brodieG <brodieG@users.noreply.github.com>
2016-05-22 15:43:30 -0400

    testup

--- tests/testthat/testthat.file.R
+++ tests/testthat/testthat.file.R
@@ -19,3 +19,36 @@ test_that("RDS", {
   expect_identical(as.character(diffPrint(f1, f2)), ref)
   expect_true(!identical(as.character(diffPrint(mx1, f2, rds=FALSE)), ref))
 })
+
+test_that("file", {
+  f1 <- tempfile()
+  f2 <- tempfile()
+  on.exit(unlink(c(f1, f2)))
+  letters2 <- letters
+  letters2[15] <- "HELLO"
+
+  writeLines(letters, f1)
+  writeLines(letters2, f2)
+
+  expect_identical(
+    as.character(diffChr(letters, letters2)),
+    as.character(diffFile(f1, f2))
+  )
+})
+
+test_that("CSV", {
+  f1 <- tempfile()
+  f2 <- tempfile()
+  on.exit(unlink(c(f1, f2)))
+
+  iris2 <- iris
+  iris2$Sepal.Length[25] <- 9.9
+
+  write.csv(iris, f1, row.names=FALSE)
+  write.csv(iris2, f2, row.names=FALSE)
+
+  expect_identical(
+    as.character(diffPrint(iris, iris2)),
+    as.character(diffCsv(f1, f2))
+  )
+})
