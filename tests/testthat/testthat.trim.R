ce475ceb0fc687d226de94365ea82203fbfeb8e5
brodieG <brodieG@users.noreply.github.com>
2016-11-10 13:47:51 -0500

    testup

--- tests/testthat/testthat.trim.R
+++ tests/testthat/testthat.trim.R
@@ -175,6 +175,7 @@ test_that("List", {
     diffobj:::strip_list_rh(l1.c, l1),
     c("[[1]]", "     [,1] [,2]", "   1    3", "   2    4", "", "$b", "$b$abc", "\"a\" \"b\" \"c\" \"d\" \"e\" \"f\" \"g\" \"h\" \"i\" \"j\" \"k\" \"l\" \"m\" \"n\" \"o\" \"p\" \"q\" \"r\" \"s\"", "\"t\" \"u\" \"v\" \"w\" \"x\" \"y\" \"z\" \"A\" \"B\" \"C\" \"D\" \"E\" \"F\" \"G\" \"H\" \"I\" \"J\" \"K\" \"L\"", "\"M\" \"N\" \"O\" \"P\" \"Q\" \"R\" \"S\" \"T\" \"U\" \"V\" \"W\" \"X\" \"Y\" \"Z\"", "", "$b[[2]]", "$b[[2]][[1]]", "     [,1] [,2]", "   4    2", "   3    1", "", "", "")
   )
+
   a <- list(list())
   aa <- list(list(), "a")
   b <- list("a", list())
@@ -186,8 +187,20 @@ test_that("List", {
     c("[[1]]", "\"a\"", "", "[[2]]", "\"b\"", "", "[[3]]", "\"c\"", "")
   )
   expect_identical(
+    diffobj:::strip_list_rh(capture.output(a), a),
+    c("[[1]]", "list()", "")
+  )
+  expect_identical(
     diffobj:::strip_list_rh(capture.output(aa), aa),
     c("[[1]]", "list()", "", "[[2]]", "\"a\"", "")
+  )
+  expect_identical(
+    diffobj:::strip_list_rh(capture.output(b), b),
+    c("[[1]]", "\"a\"", "", "[[2]]", "list()", "")
+  )
+  expect_identical(
+    diffobj:::strip_list_rh(capture.output(c), c),
+    c("[[1]]", "[[1]][[1]]", "\"a\"", "", "", "[[2]]", "\"b\"", "")
   )
 
 })
